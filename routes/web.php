7c19e2993eefb741fd598874ae64d2ff16ea0692
KarloZ7715 <ccanabalcordero70@correo.unicordoba.edu.co>
2025-10-27 18:36:13 -0500

    feat: registrar rutas CRUD de tareas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TareaController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -22,6 +23,13 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Rutas de Tareas
+    Route::get('/tareas', [TareaController::class, 'index'])->name('tareas.index');
+    Route::post('/tareas', [TareaController::class, 'store'])->name('tareas.store');
+    Route::put('/tareas/{tarea}', [TareaController::class, 'update'])->name('tareas.update');
+    Route::delete('/tareas/{tarea}', [TareaController::class, 'destroy'])->name('tareas.destroy');
+    Route::patch('/tareas/{tarea}/toggle', [TareaController::class, 'toggle'])->name('tareas.toggle');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
