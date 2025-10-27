dda61543724574232c146995117f5b64d1dfedb2
KarloZ7715 <ccanabalcordero70@correo.unicordoba.edu.co>
2025-10-27 18:37:21 -0500

    chore: agregar Index.vue placeholder y config tests

---
+++ resources/js/Pages/Tareas/Index.vue
@@ -0,0 +1,13 @@
+<script setup>
+defineProps({
+    tareas: Object,
+    filtros: Object,
+});
+</script>
+
+<template>
+    <div>
+        <h1>Tareas</h1>
+        <!-- Placeholder temporal para tests -->
+    </div>
+</template>
