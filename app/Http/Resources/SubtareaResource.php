7de50126392eb585b04a1a007f7ba79e8042806d
KarloZ7715 <ccanabalcordero70@correo.unicordoba.edu.co>
2025-10-29 16:41:40 -0500

    feat: crear SubtareaResource

---
+++ app/Http/Resources/SubtareaResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * Resource para transformar Subtareas en respuestas JSON.
+ * 
+ * Formatea los datos de subtareas para el frontend.
+ */
+class SubtareaResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'texto' => $this->texto,
+            'estado' => $this->estado,
+            'tarea_id' => $this->tarea_id,
+            'created_at' => $this->created_at?->format('Y-m-d H:i:s'),
+            'updated_at' => $this->updated_at?->format('Y-m-d H:i:s'),
+        ];
+    }
+}
